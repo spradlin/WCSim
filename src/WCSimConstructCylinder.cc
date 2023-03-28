08e7a2bce1f384d657b9970d8f3730dcc00b7524
Patrick Spradlin <patrick.spradlin@cern.ch>
2023-03-28 12:25:27 +0200

    Turn on visualziation options for cell logical volumes

--- src/WCSimConstructCylinder.cc
+++ src/WCSimConstructCylinder.cc
@@ -74,7 +74,7 @@ G4LogicalVolume* WCSimDetectorConstruction::ConstructCylinder()
   // Positions
   //-----------------------------------------------------
 
-  debugMode = false;
+  debugMode = true;
   
   WCPosition=0.;//Set the WC tube offset to zero
 
